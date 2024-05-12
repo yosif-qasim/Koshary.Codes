4ca27cb7472d9bb9f67ba4a73bbe3567532d6194
yosif-qasim <93684533+yosif-qasim@users.noreply.github.com>
2024-05-12 22:06:20 +0300

    some cleaning

--- public/index.php
+++ public/index.php
@@ -1,21 +1,4 @@
 <?php
-//
-//define('ROOT_PATH', dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR );
-//define('VIEW_PATH', dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR);
-//define('CONTROLLER_PATH', dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'controller' . DIRECTORY_SEPARATOR);
-//define('MODEL_PATH', dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'model' . DIRECTORY_SEPARATOR);
-//
-////echo "Test";
-//$page = $_GET['page'] ?? 'home';
-//
-//if($page == 'home'){
-//
-//    include VIEW_PATH . 'index.html';
-//
-//}else{
-//    echo '404';
-//}
-
 
 define('ROOT_PATH', dirname(__FILE__) . DIRECTORY_SEPARATOR );
 define('VIEW_PATH', ROOT_PATH . 'view' . DIRECTORY_SEPARATOR);
