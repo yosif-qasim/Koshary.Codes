abac938954b1a5ddb2208aaa5295bbdb4b1eb2c6
yosif-qasim <93684533+yosif-qasim@users.noreply.github.com>
2024-05-14 10:25:39 +0300

    no duplicate signup (ZeCuRiTy)

--- controller/signupController.php
+++ controller/signupController.php
@@ -6,25 +6,31 @@ class signupController
     {
         $template = new template('index');
         $template->viewPage('login', ["signupForm"], []);
-        if($_POST['postAction'] ?? 0 == 1) {
-            $this->loginAction();
+        if($_POST['postAction'] ?? 0 == 1 ) {
+            $this->signupAction();
         }
     }
 
-    function loginAction()
+    function signupAction()
     {
         $dbh = DatabaseConnection::getInstance();
         $dbc = $dbh->getConnection();
+        $username = $_POST['username'] ?? '';
+        $password = hash_hmac("sha256", $_POST['password'], "k0sh4Ry") ?? '';
 
-        if ($_POST['postAction'] ?? 0 == 1) {
-            $username = $_POST['username'] ?? '';
-            $password = hash_hmac("sha256", $_POST['password'], "k0sh4Ry") ?? '';
+        $userObj = new user($dbc);
+        $userObj->findBy('username', $username);
+
+        if (!($userObj->id)&&$_POST['postAction'] ?? 0 == 1) {
+
             $stmt = $dbc->prepare('INSERT INTO user (username, password) VALUES (?,  ?)');
             if ($stmt->execute([$username, $password])) {
                 return 'User registered successfully!';
             } else {
                 return 'Error occurred during registration.';
             }
+        }else{
+            echo "user already exist";
         }
     }
 }
